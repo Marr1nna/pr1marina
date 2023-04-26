add6e43bb92243ced1d417e9391366f8ba0b811b
student <student@B313-9>
2023-04-26 11:42:30 +0700

    Добавьте файлы проекта.

---
+++ pr1marina/Candidate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr1marina
+{
+    internal class Candidate
+    {
+        private string _name;
+        private uint _voice;
+        public string Name
+        {
+            get { return _name; }
+            private set { _name = value; }
+        }
+        public uint Voice
+        {
+            get { return _voice; }
+            private set { _voice = value; }
+        }
+        public Candidate(string name, uint voice)
+        {
+            if (name == null)
+                throw new NullReferenceException("Name");
+            Name = name;
+            Voice = voice;
+        }
+        public uint addVoice (Candidate candidate)
+        {
+            Console.WriteLine("Name:", candidate.Name);
+            Console.WriteLine("Введите количество голосов:");
+            uint voices = Convert.ToUInt32(Console.ReadLine());
+            candidate.Voice += voices;
+            return candidate.Voice;
+        }
+        void Display()
+        {
+            Console.WriteLine("Candidate");
+            Console.WriteLine($"Name:{Name}");
+            Console.WriteLine($"Voice:{Voice}");
+        }
+    }
+}
